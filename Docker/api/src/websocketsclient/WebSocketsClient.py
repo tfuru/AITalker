643fc9fd001585c7dbc41456b05c404bf6b07664
tfuru <t_furu@tf-web.jp>
2023-07-11 03:16:10 +0900

    WebSocket タイムアウトしないよう変更

--- Docker/api/src/websocketsclient/WebSocketsClient.py
+++ Docker/api/src/websocketsclient/WebSocketsClient.py
@@ -1,24 +1,26 @@
-# import asyncio
+import time
 import threading
 from websockets.sync.client import connect
+from websockets.exceptions import ConnectionClosedError
 import logging
 
 class WebSocketsClient:
     def __init__(self):
-        self.ws = connect("ws://host.docker.internal:8766")
-        logging.info(f'WebSocketsClient init { self.ws }')
+        self.WS_URI = "ws://host.docker.internal:8766"        
+        logging.info(f'WebSocketsClient init')
         self.msgs = []
         self.thread = threading.Thread(target=self.run, args=())
         self.thread.start()
 
     def run(self):
         self.isActive = True
+        self.ws = connect(self.WS_URI, open_timeout=None, close_timeout=None)
         while self.isActive:
             if self.msgs == []:
                 continue
             msg = self.msgs.pop(0)
             logging.info(f'msg: {msg}')
-            self.ws.send(msg)            
+            self.ws.send(msg)
         self.ws.close()
 
     def stop(self):
